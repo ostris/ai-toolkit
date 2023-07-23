9367089d488048f8fde982f7fca089b33d07753a
Jaret Burkett <jaretburkett@gmail.com>
2023-07-23 11:24:12 -0600

    Added example for slider training that will run as is

--- toolkit/paths.py
+++ toolkit/paths.py
@@ -4,3 +4,10 @@ TOOLKIT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CONFIG_ROOT = os.path.join(TOOLKIT_ROOT, 'config')
 SD_SCRIPTS_ROOT = os.path.join(TOOLKIT_ROOT, "repositories", "sd-scripts")
 REPOS_ROOT = os.path.join(TOOLKIT_ROOT, "repositories")
+
+
+def get_path(path):
+    # we allow absolute paths, but if it is not absolute, we assume it is relative to the toolkit root
+    if not os.path.isabs(path):
+        path = os.path.join(TOOLKIT_ROOT, path)
+    return path
