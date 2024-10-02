477788b9f519b996594b6b3c14e3cdfe2571d776
90houlaoheshang <907333918@qq.com>
2024-10-02 20:46:06 +0800

    调整`flux_inference_ui.py`文件中模型加载和Gradio界面创建的执行顺序
    将模型加载和Gradio界面创建逻辑调整到主执行块内，确保只有在直接运行该脚本时才会执行这些操作。这样的改动可以提高代码的可导入性，使得其中的函数或变量可以在其他脚本中重复利用，同时避免了在导入时不必要的资源占用。

--- flux_inference_ui.py
+++ flux_inference_ui.py
@@ -8,13 +8,9 @@ from io import BytesIO
 # 预训练模型和 LoRA 权重路径
 model_id = "black-forest-labs/FLUX.1-dev"
 lora_dir = "antas/fenglin-flux-lora"
-
-# 加载模型
-pipe = FluxPipeline.from_pretrained(model_id, torch_dtype=torch.bfloat16)
-pipe.load_lora_weights(lora_dir, weight_name="fenglin-flux-lora.safetensors")
-pipe.enable_model_cpu_offload()
 # trigger word
 trigger_word = "fenglin"
+
 
 def generate_image(prompt, seed, num_inference_steps):
     """
@@ -38,18 +34,24 @@ def generate_image(prompt, seed, num_inference_steps):
 
     return image  # 直接返回图片对象
 
-# 创建 Gradio 界面
-iface = gr.Interface(
-    fn=generate_image,
-    inputs=[
-        gr.Textbox(label="提示文本", placeholder="请输入提示文本"),
-        gr.Number(label="随机种子", value=1, precision=0, info="只能输入-1（随机）或正整数，固定的提示文本和固定的正数随机种子会生成同样的图片"),
-        gr.Slider(label="推理步数", minimum=1, maximum=100, step=1, value=32)
-    ],
-    outputs=gr.Image(type="pil", format="png"),  # 使用 PIL Image 类型并指定格式为 png
-    title="图像生成器",
-    description="请输入提示文本、随机种子及推理步数以生成图片。"
-)
 
-# 启动 Gradio 应用
-iface.launch(share=True)
+if __name__ == '__main__':
+    # 加载模型
+    pipe = FluxPipeline.from_pretrained(model_id, torch_dtype=torch.bfloat16)
+    pipe.load_lora_weights(lora_dir, weight_name="fenglin-flux-lora.safetensors")
+    pipe.enable_model_cpu_offload()
+    # 创建 Gradio 界面
+    iface = gr.Interface(
+        fn=generate_image,
+        inputs=[
+            gr.Textbox(label="提示文本", placeholder="请输入提示文本"),
+            gr.Number(label="随机种子", value=1, precision=0,
+                      info="只能输入-1（随机）或正整数，固定的提示文本和固定的正数随机种子会生成同样的图片"),
+            gr.Slider(label="推理步数", minimum=1, maximum=100, step=1, value=32)
+        ],
+        outputs=gr.Image(type="pil", format="png"),  # 使用 PIL Image 类型并指定格式为 png
+        title="图像生成器",
+        description="请输入提示文本、随机种子及推理步数以生成图片。"
+    )
+    # 启动 Gradio 应用
+    iface.launch(share=True)
