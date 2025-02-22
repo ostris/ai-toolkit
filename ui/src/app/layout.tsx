77a5e013012e606735caf354d177dee5e9fbd762
Jaret Burkett <jaretburkett@gmail.com>
2025-02-22 09:05:55 -0700

    Added proper icon

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="AI-Toolkit" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider>
           <div className="flex h-screen bg-gray-950">
