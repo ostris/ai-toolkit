c90615f8bb95e84e3cb3b0829e1024332f0d5e06
Jaret Burkett <jaretburkett@gmail.com>
2025-04-17 09:00:10 -0600

    Add model hooks to polarity loss

--- toolkit/guidance.py
+++ toolkit/guidance.py
@@ -453,12 +453,14 @@ def get_guided_loss_polarity(
             noise,
             timesteps
         ).detach()
+        conditional_noisy_latents = sd.condition_noisy_latents(conditional_noisy_latents, batch)
 
         unconditional_noisy_latents = sd.add_noise(
             unconditional_latents,
             noise,
             timesteps
         ).detach()
+        unconditional_noisy_latents = sd.condition_noisy_latents(unconditional_noisy_latents, batch)
 
         # double up everything to run it through all at once
         cat_embeds = concat_prompt_embeds([conditional_embeds, conditional_embeds])
