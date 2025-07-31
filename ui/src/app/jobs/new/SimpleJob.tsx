53add7708300effc86515f322a9c4c36fe2cbc6f
nflamously <david.dembinski.1996@gmail.com>
2025-07-31 18:56:19 +0200

    feat: add automagic as as a select value option in the ui under SimpleJob

--- ui/src/app/jobs/new/SimpleJob.tsx
+++ ui/src/app/jobs/new/SimpleJob.tsx
@@ -322,6 +322,7 @@ export default function SimpleJob({
                   options={[
                     { value: 'adamw8bit', label: 'AdamW8Bit' },
                     { value: 'adafactor', label: 'Adafactor' },
+                    { value: 'automagic', label: 'Automagic' },
                   ]}
                 />
                 <NumberInput
