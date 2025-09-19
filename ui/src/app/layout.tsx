01b914dc675379b11c5a2fd5c40640fbfd2b06b1
Mikoto01 <kennychoyck@live.com.my>
2025-09-19 14:19:42 +0900

    Fix manifest path

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -16,6 +16,7 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Ostris - AI Toolkit',
   description: 'A toolkit for building AI things.',
+  manifest: `${BASE_PATH}/manifest.json`,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
