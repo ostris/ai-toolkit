4b3f3092d1e332316c8922e26cdaadbb0cb4d3ec
Greg Schoeninger <greg.schoeninger@gmail.com>
2025-09-29 17:36:52 -0700

    check if control images is none before passing into encode prompt

--- extensions_built_in/diffusion_models/qwen_image/qwen_image_edit_plus.py
+++ extensions_built_in/diffusion_models/qwen_image/qwen_image_edit_plus.py
@@ -183,10 +183,12 @@ class QwenImageEditPlusModel(QwenImageModel):
 
                 print(f"end control_images[i].shape {control_images[i].shape}")
 
-        control_image = control_images[0]
+        if control_images is not None and len(control_images) > 0:
+            control_images = control_images[0]
+
         prompt_embeds, prompt_embeds_mask = self.pipeline.encode_prompt(
             prompt,
-            image=control_image,
+            image=control_images,
             device=self.device_torch,
             num_images_per_prompt=1,
         )
