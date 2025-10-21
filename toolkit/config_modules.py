db766176e24dc4bccc8ce8eba138717773129b04
kschmid <kschmid@nvidia.com>
2025-10-21 12:31:55 +1100

    div

--- toolkit/config_modules.py
+++ toolkit/config_modules.py
@@ -4,7 +4,7 @@ from typing import List, Optional, Literal, Tuple, Union, TYPE_CHECKING, Dict
 import random
 
 import torch
-import torchaudio
+
 
 from toolkit.prompt_utils import PromptEmbeds
 
@@ -1143,6 +1143,7 @@ class GenerateImageConfig:
             else:
                 raise ValueError(f"Unsupported video format {self.output_ext}")
         elif self.output_ext in ['wav', 'mp3']:
+            import torchaudio
             # save audio file
             torchaudio.save(
                 self.get_image_path(count, max_count), 
