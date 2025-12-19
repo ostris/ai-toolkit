Fix call to accelerator broadcast

jobs/process/BaseSDTrainProcess.py
@@ -2174,7 +2174,7 @@ class BaseSDTrainProcess(BaseTrainProcess):
                     stop_early = self.oxen_logger.is_stopping()
 
                 stop_early = torch.tensor(int(stop_early), device=self.accelerator.device)
-                stop_early = self.accelerator.broadcast(stop_early, from_process=0)
+                stop_early = self.accelerator.utils.broadcast(stop_early, from_process=0)
 
                 self.accelerator.wait_for_everyone()
 