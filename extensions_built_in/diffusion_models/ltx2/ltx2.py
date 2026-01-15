9848de79463a08611d0a8acbe03f744e84276dc1
Jaret Burkett <jaretburkett@gmail.com>
2026-01-14 17:27:01 -0700

    Fix issue with ltx cached latents if there is no audio.

--- extensions_built_in/diffusion_models/ltx2/ltx2.py
+++ extensions_built_in/diffusion_models/ltx2/ltx2.py
@@ -838,7 +838,7 @@ class LTX2Model(BaseModel):
                     batch_size,
                     num_channels_latents=num_channels_latents_audio,
                     num_mel_bins=num_mel_bins,
-                    num_frames=batch.tensor.shape[1],
+                    num_frames=batch.dataset_config.num_frames,
                     frame_rate=frame_rate,
                     sampling_rate=self.pipeline.audio_sampling_rate,
                     hop_length=self.pipeline.audio_hop_length,
