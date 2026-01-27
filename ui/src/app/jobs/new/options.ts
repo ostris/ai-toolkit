2db090144a8e6b568104ec5808a2f957545d9c50
Jaret Burkett <jaretburkett@gmail.com>
2026-01-27 09:34:46 -0700

    Add support for Z-Image

--- ui/src/app/jobs/new/options.ts
+++ ui/src/app/jobs/new/options.ts
@@ -584,6 +584,25 @@ export const modelArchs: ModelArch[] = [
     additionalSections: ['model.low_vram', 'model.layer_offloading', 'model.assistant_lora_path'],
   },
   {
+    name: 'zimage',
+    label: 'Z-Image',
+    group: 'image',
+    defaults: {
+      // default updates when [selected, unselected] in the UI
+      'config.process[0].model.name_or_path': ['Tongyi-MAI/Z-Image', defaultNameOrPath],
+      'config.process[0].model.quantize': [true, false],
+      'config.process[0].model.quantize_te': [true, false],
+      'config.process[0].model.low_vram': [true, false],
+      'config.process[0].train.unload_text_encoder': [false, false],
+      'config.process[0].sample.sampler': ['flowmatch', 'flowmatch'],
+      'config.process[0].train.noise_scheduler': ['flowmatch', 'flowmatch'],
+      'config.process[0].train.timestep_type': ['weighted', 'sigmoid'],
+      'config.process[0].model.qtype': ['qfloat8', 'qfloat8'],
+    },
+    disableSections: ['network.conv'],
+    additionalSections: ['model.low_vram', 'model.layer_offloading'],
+  },
+  {
     name: 'zimage:deturbo',
     label: 'Z-Image De-Turbo (De-Distilled)',
     group: 'image',
