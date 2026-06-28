b9bb68a940e7b2fdbe3d7fe804d7edc572e7faf4
PlagueKind <284434812+PlagueKind@users.noreply.github.com>
2026-06-28 22:00:55 +0200

    progressbar use fatalis code

--- toolkit/progress_bar.py
+++ toolkit/progress_bar.py
@@ -1,9 +1,12 @@
 from tqdm import tqdm
-import time
 
 
 class ToolkitProgressBar(tqdm):
     def __init__(self, *args, **kwargs):
+        kwargs.setdefault("mininterval", 1.0)
+        kwargs.setdefault("maxinterval", 10.0)
+        kwargs.setdefault("miniters", 1)
+        kwargs.setdefault("smoothing", 0.05)
         super().__init__(*args, **kwargs)
         self.paused = False
         self.last_time = self._time()
