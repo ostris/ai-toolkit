2e50bf9f88bcec95166eafbf5c358593319ab1ec
Rydén Johan <johan.ryden@bostad.uppsala.se>
2026-07-17 14:14:22 +0200

    run: preserve original error when job creation fails

--- run.py
+++ run.py
@@ -107,6 +107,7 @@ def main():
         print_acc(f"Running {len(config_file_list)} job{'' if len(config_file_list) == 1 else 's'}")
 
     for config_file in config_file_list:
+        job = None
         try:
             job = get_job(config_file, args.name)
             job.run()
@@ -116,7 +117,8 @@ def main():
             print_acc(f"Error running job: {e}")
             jobs_failed += 1
             try:
-                job.process[0].on_error(e)
+                if job is not None:
+                    job.process[0].on_error(e)
             except Exception as e2:
                 print_acc(f"Error running on_error: {e2}")
             if not args.recover:
@@ -124,7 +126,8 @@ def main():
                 raise e
         except KeyboardInterrupt as e:
             try:
-                job.process[0].on_error(e)
+                if job is not None:
+                    job.process[0].on_error(e)
             except Exception as e2:
                 print_acc(f"Error running on_error: {e2}")
             if not args.recover:
