5ff8a0435ab136cc2f1f28446945ad4d0f8d6c1c
Jaret Burkett <jaretburkett@gmail.com>
2026-08-09 14:56:39 -0600

    Make v1 the default training adapter

--- ui/src/app/jobs/new/options.tsx
+++ ui/src/app/jobs/new/options.tsx
@@ -735,7 +735,7 @@ export const modelArchs: ModelArch[] = [
       'config.process[0].datasets[x].num_frames': [39, undefined],
       'config.process[0].datasets[x].auto_frame_count': [true, undefined],
       'config.process[0].model.assistant_lora_path': [
-        'ostris/minimax_h3_training_adapter/minimax_h3_training_adapter_alpha.safetensors',
+        'ostris/minimax_h3_training_adapter/minimax_h3_training_adapter_v1.safetensors',
         undefined,
       ],
     },
